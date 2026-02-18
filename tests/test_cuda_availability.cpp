b2be0fa44a9f9ee70dbe1720dc215ef57a4f6424
Erik Stern <erik.stern@outlook.de>
2026-02-18 19:39:45 +0100

    Add cuda availability test.

---
+++ tests/test_cuda_availability.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+TEST(CudaTest, DeviceZeroIsAvailable) {
+    CUdevice cuDevice;
+    cuDeviceGet(&cuDevice, 0);
+    CUcontext cuContext;
+    cuCtxCreate(&cuContext, CU_CTX_SCHED_SPIN, cuDevice);
+    cuCtxDestroy(cuContext);
+}
